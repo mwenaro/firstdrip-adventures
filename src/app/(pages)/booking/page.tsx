d92a14e7d125d59c62e33c82d52cfe03d428ea70
Mwero Abdalla <mweroabdalla@gmail.com>
2025-02-09 12:41:01 +0300

    feat: add tours feature

--- src/app/(pages)/booking/page.tsx
+++ src/app/(pages)/booking/page.tsx
@@ -2,8 +2,8 @@ import { BookingForm } from "@/components/custom/BookingForm";
 
 export default function BookingPage() {
   return (
-    <>
+    <div className="pt-20">
       <BookingForm destination={undefined} />
-    </>
+    </div>
   );
 }
