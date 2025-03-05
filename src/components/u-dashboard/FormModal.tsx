633fa92142a1f1f1fc7724d61c40912592234108
= <=>
2025-03-05 08:23:16 +0300

    fix : bug-fix in u-dashboard

--- src/components/u-dashboard/FormModal.tsx
+++ src/components/u-dashboard/FormModal.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 import Image from "next/image";
-import { useState } from "react";
+import { ReactElement, useState } from "react";
 
 // USE LAZY LOADING
 
@@ -17,7 +17,7 @@ const StudentForm = dynamic(() => import("./forms/StudentForm"), {
 });
 
 const forms: {
-  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+  [key: string]: (type: "create" | "update", data?: any) => ReactElement;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
   student: (type, data) => <StudentForm type={type} data={data} />
