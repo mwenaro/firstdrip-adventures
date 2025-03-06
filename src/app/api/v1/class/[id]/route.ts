d8145507c17eaadb74fc36492f81c780005da295
= <=>
2025-03-06 14:16:52 +0300

    fix : bug-fix in /route/class

--- src/app/api/v1/class/[id]/route.ts
+++ src/app/api/v1/class/[id]/route.ts
@@ -1,13 +1,12 @@
 import { dbCon } from "@/libs/mongoose/dbCon";
 import { ClassModel } from "@/models/Class";
 import { NextRequest, NextResponse } from "next/server";
-type Query = { params: { id: string } };
 
-export async function GET(req: NextRequest, { params: { id } }: any) {
-console.log(req)
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  console.log(req);
   try {
     await dbCon();
-    const fetchedClass = await ClassModel.findOne({ _id: id });
+    const fetchedClass = await ClassModel.findById(params.id);
     if (!fetchedClass)
       return NextResponse.json(
         { success: false, message: "Not Found" },
@@ -23,17 +22,22 @@ console.log(req)
   }
 }
 
-export async function PUT(req: NextRequest, { params: { id } }: Query) {
-console.log(req)
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  console.log(req);
   const body = await req.json();
   try {
     await dbCon();
-    const updatedClass = await ClassModel.findByIdAndUpdate(id, body);
+    const updatedClass = await ClassModel.findByIdAndUpdate(params.id, body, {
+      new: true,
+      runValidators: true,
+    });
+
     if (!updatedClass)
       return NextResponse.json(
         { success: false, message: "Not Found" },
         { status: 404 }
       );
+
     return NextResponse.json({ success: true, data: updatedClass });
   } catch (error: any) {
     return NextResponse.json(
@@ -43,15 +47,20 @@ console.log(req)
   }
 }
 
-export async function DELETE(req: NextRequest, { params: { id } }: Query) {
-console.log(req)
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  console.log(req);
   try {
     await dbCon();
-    const deletedClasss = await ClassModel.findByIdAndDelete(id);
+    const deletedClass = await ClassModel.findByIdAndDelete(params.id);
+    if (!deletedClass)
+      return NextResponse.json(
+        { success: false, message: "Not Found" },
+        { status: 404 }
+      );
 
     return NextResponse.json(
-      { success: true, data: deletedClasss },
-      { status: 201 }
+      { success: true, data: deletedClass },
+      { status: 200 }
     );
   } catch (error: any) {
     return NextResponse.json(
