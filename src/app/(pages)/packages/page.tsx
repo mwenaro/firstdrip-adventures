776e28b348ced2f9520daa77517c5f82078f85cb
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-05-26 17:00:29 +0300

    chore: add Mombasa based packages

--- src/app/(pages)/packages/page.tsx
+++ src/app/(pages)/packages/page.tsx
@@ -3,7 +3,7 @@ import { TourPackageList } from "@/components/packages/TourPackageList";
 export default function TourPackagesPage() {
   return <div className="min-h-screen bg-gray-50 mt-16 pt-6">
     <h2 className="text-3xl font-bold text-center my-6 md:text-4xl">Safari Packages</h2>
-    <TourPackageList limit={2} showMoreBtn={true} />
+    <TourPackageList limit={5} showMoreBtn={true} />
 
   </div>
 }
